77284952d93cca358142a7134e31663d2370db7b
Chikage <chikage@939.me>
2021-12-31 23:55:05 +0800

    删除重复的QOS

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
-.
+
 rm -rf package/lean/luci-theme-argon
 git clone  -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone  https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
